ac2cb5c3f7b1382ae526f39f81369b888cd4e80a
dvasin <dvasin@wavesplatform.com>
2021-10-17 11:09:58 +0300

    Add build all script

---
+++ build-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Building all modules..."
+
+declare -a modules=(
+    "nfts-auth" 
+    "nfts-polygon-adapter" 
+    "nfts-stripe-adapter"    
+    "nfts-warehouse"
+)
+
+function build_docker() {
+    cd ${1}
+    sh build-docker.sh
+    cd ..
+}
+
+for (( i=0; i<${#modules[@]}; i++ ));
+do
+  let "INDEX=${i}+1"
+  echo "[${INDEX}/${#modules[@]}] Building: ${modules[$i]}"
+  build_docker ${modules[$i]}
+done
