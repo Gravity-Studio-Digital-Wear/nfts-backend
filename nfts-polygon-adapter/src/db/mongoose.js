080da1ebd713e41322238e4fb62c3f27b1b90a10
Vladimir Safronov <34181291+savauln@users.noreply.github.com>
2022-07-11 03:12:19 +0400

    Add github actions pipeline (#8)
    * Test pipeline
    
    * Test pipeline
    
    * Test pipeline
    
    * Test pipeline
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * FIx
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    Co-authored-by: Vladimir Safronov <vsafronov@web3tech.ru>

--- nfts-polygon-adapter/src/db/mongoose.js
+++ nfts-polygon-adapter/src/db/mongoose.js
@@ -3,28 +3,12 @@ const mongoose = require('mongoose');
 let initialized = false
 
 const init = async() => {
-    if (initialized) {
-        return
-    }
-    initialized = true
-    if (!process.env.MONGO_USER) {
-        console.error(`Define MONGO_USER`)
-        process.exit(0)
-    }
-    if (!process.env.MONGO_PASSWORD) {
-        console.error(`Define MONGO_PASSWORD`)
-        process.exit(0)
-    }
-    if (!process.env.MONGO_HOST) {
-        console.error(`Define MONGO_HOST`)
-        process.exit(0)
-    }
-    if (!process.env.MONGO_DB) {
-        console.error(`Define MONGO_DB`)
+    if (!process.env.MONGO_URL) {
+        console.error(`Define MONGO_URL`)
         process.exit(0)
     }
     console.log(`Connecting to db...`)
-    await mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:27017/${process.env.MONGO_DB}`)
+    await mongoose.connect(`${process.env.MONGO_URL}`)
     console.log(`Database connected`)
 }
 
